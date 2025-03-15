818840a5bdbb1f3440a4b7cb281ff170e9d9a4c2
Opal <opaltales_@windowslive.com>
2025-03-15 11:30:24 +0700

    502

--- srcs/requirements/mariadb/tools/script_bash_mariadb.sh
+++ srcs/requirements/mariadb/tools/script_bash_mariadb.sh
@@ -3,25 +3,25 @@
 echo "Hello maria"
 /etc/script_bash_mariadb.d/mariadb start
 
-if [ -d "/var/lib/mysql/$MRDB_NAME" ]
+if [-d "/var/lib/mysql/$MRDB_NAME"]
 then
 	echo "Database exists"
 else
 
-mysql_secure_installation << EOF
+	mysql_secure_installation << EOF
 
-Y
-Y
-$MRDB_ROOT
-$MRDB_ROOT
-Y
-N
-Y
-Y
-EOF
+	Y
+	Y
+	$MRDB_ROOT
+	$MRDB_ROOT
+	Y
+	N
+	Y
+	Y
+	EOF
 
-echo "GRANT ALL ON *.* TO 'root'@'%' IDENTIFIED BY '$MRDB_ROOT'; FLUSH PRIVILEGES;" | mysql -uroot
-echo "CREATE DATABASE IF NOT EXISTS $MRDB_NAME; GRANT ALL ON $MRDB_NAME.* TO '$MRDB_USER'@'%' IDENTIFIED BY '$MRDB_PASS'; FLUSH PRIVILEGES;" | mysql -uroot
+	echo "GRANT ALL ON *.* TO 'root'@'%' IDENTIFIED BY '$MRDB_ROOT'; FLUSH PRIVILEGES;" | mysql -uroot
+	echo "CREATE DATABASE IF NOT EXISTS $MRDB_NAME; GRANT ALL ON $MRDB_NAME.* TO '$MRDB_USER'@'%' IDENTIFIED BY '$MRDB_PASS'; FLUSH PRIVILEGES;" | mysql -uroot
 
 fi
 
